2c3ff95ef0ddc26aae73b0997438d7b984072234
scjonatas <scjonatas@gmail.com>
2013-05-07 14:17:25 -0300

    Tutorial parte 4

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 var app = {
     
+    renderHomeView: function() {
+	var html =
+		"<div class='header'><h1>Home</h1></div>" +
+		"<div class='search-view'>" +
+		"<input class='search-key'/>" +
+		"<ul class='employee-list'></ul>" +
+		"</div>"
+	$('body').html(html);
+	$('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+    
     showAlert: function (message, title) {
 	if (navigator.notification) {
 	    navigator.notification.alert(message, null, title, 'OK');
@@ -22,11 +33,10 @@ var app = {
     },
 
     initialize: function() {
-        var self = this;
+	var self = this;
 	this.store = new MemoryStore(function() {
-	    self.showAlert('Hello World!', 'Info');
+	    self.renderHomeView();
 	});
-	$('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
